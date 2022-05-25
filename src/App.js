1528b074fc9e98f20b2c1d9a299cb76fd2d6d938
YashJain <yjain3.436@gmail.com>
2022-05-25 14:10:27 +0530

    updated with custom domain

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload the yashGG.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">MrRobot</header>
+      <hr
+        style={{
+          color: "#000000",
+          backgroundColor: "#000000",
+          height: 0.2,
+          borderColor: "#000000",
+        }}
+      />
     </div>
   );
 }
